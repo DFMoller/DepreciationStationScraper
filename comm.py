9c61d1c3d0c2d13abcefefbd0b6b3f0d818c6fa3
DFMoller <dfmoller@gmail.com>
2022-03-27 11:50:08 +0200

    Restored Repo to working state after merge

--- comm.py
+++ comm.py
@@ -3,18 +3,11 @@ from pandas import DataFrame
 from datetime import date, datetime
 # from scraper import log
 
-<<<<<<< HEAD
 class maxRetries(Exception):
     pass
 
 def log(message):
-    # /home/pi/Apps/DepreciationStationScraper/
     with open("/home/pi/Apps/DepreciationStationScraper/log/" + datetime.now().strftime("%Y-%m-%d") + ".txt", "a") as f:
-=======
-def log(message):
-    # /home/pi/Apps/DepreciationStationScraper/
-    with open("log/" + datetime.now().strftime("%Y-%m-%d") + ".txt", "a") as f:
->>>>>>> ad327f40d172fe9b03283d136376dd70c2638db1
         print(message)
         print(message, file=f)
 
@@ -25,8 +18,6 @@ years = [2015, 2016, 2017, 2018, 2019, 2020]
 
     
 def addSearches():
-    # /home/pi/Apps/DepreciationStationScraper/
-<<<<<<< HEAD
     add_search_error_count = 0
     for color in colors:
         for year in years:
@@ -34,11 +25,6 @@ def addSearches():
                 if add_search_error_count > 10:
                     log(f'Adding Search for color: {color} and year: {year} exceeded 10 tries. Raising custom exception!')
                     raise maxRetries(f'Adding Search for color: {color} and year: {year} exceeded 10 failures')
-=======
-    for color in colors:
-        for year in years:
-            while True:
->>>>>>> ad327f40d172fe9b03283d136376dd70c2638db1
                 try:
                     response = requests.post(BASE + "addSearch", {"color": color, "year": year})
                     log(response.json())
@@ -48,38 +34,16 @@ def addSearches():
                     log("Request Causing the error: Add Search for " + color + " cars from " + str(year))
                 except ValueError as err:  # ValueError includes simplejson.decoder.JSONDecodeError
                     log("JSON Decode Error when adding Search: " + str(err))
-<<<<<<< HEAD
                     add_search_error_count += 1
 
 def getSearches():
     while True:
         try:
             response = requests.get(BASE + "getSearches")
-=======
-
-def getSearches():
-    while True:
-        try:
-            response = requests.get(BASE + "getSearches")
             return response.json()
             break
         except (requests.ConnectionError, requests.Timeout) as exception:
             log("Error: " + str(exception))
-            log("Request Causing the error: GetSearches()")
-        except ValueError as err:
-            log("JSON Decode Error when fetching Searches: " + str(err))
-
-def postReadings(post_data):
-    json_list = json.dumps(post_data)
-    while True:
-        try:
-            response = requests.post(BASE + "addReadings", {"data": json_list})
->>>>>>> ad327f40d172fe9b03283d136376dd70c2638db1
-            return response.json()
-            break
-        except (requests.ConnectionError, requests.Timeout) as exception:
-            log("Error: " + str(exception))
-<<<<<<< HEAD
             log("Request Causing the error: GetSearches()")
         except ValueError as err:
             log("JSON Decode Error when fetching Searches: " + str(err))
@@ -118,33 +82,6 @@ def postHistory(data):
             print(sorted_df)
             print("Median Value: " + str(median_value))
     
-=======
-            log("Request Causing the error: PostReadings() -> search_id = " + str(post_data[0]['search_id']))
-        except ValueError as err:
-            log("JSON Decode Error when posting Readings: " + str(err))
-            log("Request Causing the error: PostReadings() -> search_id = " + str(post_data[0]['search_id']))
-            
-
-def postHistory(data):
-    
-    final_data = {}
-    for search_id in data:
-        line = []
-        for node in data[search_id]:
-            instance = (node["value"], node["mileage"], node["date"])
-            line.append(instance)
-        df = DataFrame(line, columns=["value", "mileage", "date"])
-        sorted_df = df.sort_values("value")
-        median_value = sorted_df["value"].median()
-        final_data[search_id] = {
-            "date": date.today().strftime("%d/%m/%Y"),
-            "median_value": int(median_value)
-        }
-        if search_id == 1:
-            print(sorted_df)
-            print("Median Value: " + str(median_value))
-    
->>>>>>> ad327f40d172fe9b03283d136376dd70c2638db1
     while True:
         try:
             response = requests.post(BASE + "addHistory", {"data": json.dumps(final_data)})
@@ -164,10 +101,6 @@ def deleteOldEntries():
     while True:
         try:
             response = requests.delete(BASE + "deleteOldEntries")
-<<<<<<< HEAD
-=======
-            log(response.json())
->>>>>>> ad327f40d172fe9b03283d136376dd70c2638db1
             return response.json()
             break
         except (requests.ConnectionError, requests.Timeout) as exception:
